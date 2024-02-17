f341fbb281cf12a21b5672fd4dcf319aa98aad25
bogdan <b.pyanzin@mail.ru>
2024-02-17 16:00:04 +0700

    ver1

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from time import sleep
 import Sendler.log as log
-from os import path, remove,listdir
+from os import path, remove, listdir
+
 DELAY = 60
-MESSAGE= 'GUI'
-DIRECTORY='EmailsToSend'
+MESSAGE = 'GUI'
+DIRECTORY = 'EmailsToSend'
 if __name__ == "__main__":
-    while(True):
-        files=[file for file in listdir(DIRECTORY) if file.endswith('.txt')]
-        log.send(MESSAGE,files)
+    while True:
+        files = [file for file in listdir(DIRECTORY) if file.endswith('.txt')]
+        log.send(MESSAGE, files)
         for file in files:
             if path.exists(f'{DIRECTORY}/{file}'):
                 remove(f'{DIRECTORY}/{file}')
